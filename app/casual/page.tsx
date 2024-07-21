5d49dca5de06f130b4ce30b2145d4bf07f493ab1
Kunal Khairnar <95234444+Kunal-Khairnar-05@users.noreply.github.com>
2024-07-21 17:52:37 +0530

    Update : casual life bg change

--- app/casual/page.tsx
+++ app/casual/page.tsx
@@ -10,8 +10,8 @@ import Guitar from "./guitar";
 function page() {
   return (
     <main className="relative bg-yellow-100 dark:bg-yellow-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="h-screen w-full bg-yellow-400 dark:bg-yellow-400 bg-grid-white/[0.08] flex justify-center items-center absolute top-0 left-0">
-        <div className="absolute pointer-events-none -inset-2 flex justify-center items-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_10%,white)]">
+      <div className="h-screen w-full bg-yellow-300 dark:bg-yellow-400 bg-grid-white/[0.08] flex justify-center items-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none -inset-2 flex justify-center items-center bg-yellow-500 [mask-image:radial-gradient(ellipse_at_center,transparent_10%,white)]">
           </div>
       </div>
 
@@ -28,7 +28,7 @@ function page() {
       <div className="my-10 z-10 flex justify-center">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center ">
           <TextGenerateEffect
-            className="text-center text-white text-[20px] md:text-4xl lg:text-5xl flex justify-center"
+            className="text-center text-cyan-600 text-[20px] md:text-4xl lg:text-5xl flex justify-center"
             words={`Peek in my`}
           />
           <TextGenerateEffect
